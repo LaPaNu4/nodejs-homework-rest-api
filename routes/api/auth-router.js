206b8f1305d6c032df30f39b3abeebaa20b65bfb
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-10-02 17:27:33 +0300

    verification api

--- routes/api/auth-router.js
+++ routes/api/auth-router.js
@@ -12,7 +12,9 @@ const userSigninSchema = validateBody(userSchema.userSigninSchema);
 authRouter.post("/users/register", userSugnupValidate, authControler.signup);
 authRouter.post("/users/login", userSigninSchema, authControler.signin);
 authRouter.get("/users/current", authenticate, authControler.getCurrent);
+authRouter.get("/users/verify/:verificationToken", authControler.verification);
 authRouter.post("/users/logout", authenticate, authControler.logout);
+authRouter.post("/users/verify", authControler.verifyResend);
 authRouter.patch("/users", authenticate, authControler.subscription);
 authRouter.patch(
   "/users/avatars",
